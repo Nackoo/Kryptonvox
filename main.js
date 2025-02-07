2636a8e5ff16514a4dcc4d0c0544853d2225621d
Nackoo <nacko9921@gmail.com>
2025-02-07 17:23:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 function fetchAndApplyScript(scriptUrl) {
-
-    const timestamp = new Date().getTime();
-    const updatedScriptUrl = `${scriptUrl}?v=${timestamp}`;
-
-    const scriptElement = document.createElement('script');
-    scriptElement.src = updatedScriptUrl;
-    scriptElement.type = 'text/javascript';
-
-    document.head.appendChild(scriptElement);
-
-    scriptElement.onload = () => console.log(`Script from ${updatedScriptUrl} successfully loaded and applied.`);
-    scriptElement.onerror = () => console.error(`Failed to load script from ${updatedScriptUrl}.`);
+	const timestamp = new Date().getTime();
+	const updatedScriptUrl = `${scriptUrl}?v=${timestamp}`;
+	const scriptElement = document.createElement('script');
+	scriptElement.src = updatedScriptUrl;
+	scriptElement.type = 'text/javascript';
+	scriptElement.async = true;
+	document.head.appendChild(scriptElement);
+	scriptElement.onload = () =>
+		console.log(`Script from ${updatedScriptUrl} successfully loaded.`);
+	scriptElement.onerror = () =>
+		console.error(`Failed to load script from ${updatedScriptUrl}.`);
 }
-
 fetchAndApplyScript('https://kryptonvox.netlify.app/main.js');
