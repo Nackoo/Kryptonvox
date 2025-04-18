1bb15754267341b8929e34b26d0c9bd015f34641
Nackoo <nacko9921@gmail.com>
2025-04-18 18:21:16 +0800

    Update emojiPack.js

--- emojiPack.js
+++ emojiPack.js
@@ -1,6 +1,6 @@
 //add this below your main.js code or separately
 
 document.head.appendChild(Object.assign(document.createElement('script'), { 
-    src: 'https://kryptonvox.netlify.app/emojiPack.js?v=' + Date.now(), 
+    src: 'https://kryptonvox.netlify.app/emojiPack.js?v=' + Date.now(),
     async: true 
 }));
