f73f5b765c76c370e63e4c46716d459b63cafb80
Nackoo <nacko9921@gmail.com>
2026-02-16 00:33:43 +0800

    Delete historynotes.js

--- historynotes.js
+++
@@ -1,6 +0,0 @@
-//add this on your javascript injector
-
-document.head.appendChild(Object.assign(document.createElement('script'), { 
-    src: 'https://kryptonvox.netlify.app/historynotes.js?v=' + Date.now(), 
-    async: true 
-}));
