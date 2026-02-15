0ba17c931778edf113ce6d3d6582e52767a9f370
Nackoo <nacko9921@gmail.com>
2026-02-16 00:33:36 +0800

    Delete emojiPack.js

--- emojiPack.js
+++
@@ -1,6 +0,0 @@
-//add this with/separately from the main.js code 
-
-document.head.appendChild(Object.assign(document.createElement('script'), { 
-    src: 'https://kryptonvox.netlify.app/emojiPack.js?v=' + Date.now(),
-    async: true 
-}));
